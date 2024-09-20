af61649b18421e126824e5bb84f833f6d7625a32
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2024-09-20 22:31:44 +0530

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.hazrat.islam24"
         minSdk = 26
         targetSdk= 34
-        versionCode = 71
-        versionName = "1.6.9"
+        versionCode = 72
+        versionName = "1.6.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
