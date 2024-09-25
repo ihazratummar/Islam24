40ef368c25b83443e85e383aad7403399f61e53c
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2024-09-25 07:36:12 +0530

    Change Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.hazrat.islam24"
         minSdk = 26
         targetSdk= 34
-        versionCode = 72
-        versionName = "1.6.10"
+        versionCode = 73
+        versionName = "1.6.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
