76f8bcd7ae8d3b33428918e464d87bbb24d546d9
ihazratummar <hazratummar9@gmail.com>
2025-01-14 15:55:50 +0530

    Version Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.hazrat.islam24"
         minSdk = 26
         targetSdk= 35
-        versionCode = 81
-        versionName = "2.0.0"
+        versionCode = 82
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
