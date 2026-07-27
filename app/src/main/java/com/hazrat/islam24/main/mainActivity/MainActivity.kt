fff94ceb0a957c539d124cb3228804d9c049b626
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2026-07-27 09:33:12 +0530

    feat: import SystemBarStyle to support edge-to-edge configuration in MainActivity

--- app/src/main/java/com/hazrat/islam24/main/mainActivity/MainActivity.kt
+++ app/src/main/java/com/hazrat/islam24/main/mainActivity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
